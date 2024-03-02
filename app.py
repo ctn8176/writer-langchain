d488781433e783ca8db1652e2b23d88f98c58911
Christiana Nguyen <113950084+ctn8176@users.noreply.github.com>
2024-03-03 04:53:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,7 @@ def main():
     st.set_page_config(page_title="Writer <> Langchain Workflows", page_icon=":rocket:")
     st.title('Writer <> Langchain Workflows :rocket:')
     st.subheader("How can I help?")
-
-    option = st.selectbox("Select an option:", ["Create Blog Post", "Chat with PDF Chatbot"])
-
-    if option == "Create Blog Post":
-        blog_creation()
-    elif option == "Chat with PDF Chatbot":
-        chatbot()
+    chatbot()
 
 if __name__ == '__main__':
     main()
