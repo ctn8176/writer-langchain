4553b0a6b0437a8c7da1576b66677f324bf6365e
Christiana Nguyen <113950084+ctn8176@users.noreply.github.com>
2024-03-04 09:22:14 +0700

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -14,6 +14,8 @@ import os
 
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+os.environ['WRITER_API_KEY'] = st.secrets['WRITER_API_KEY']
+os.environ['WRITER_ORG_ID'] = st.secrets['WRITER_ORG_ID]
 
 
 def get_pdf_text(pdf_docs):
